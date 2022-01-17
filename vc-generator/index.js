bc581df6e72c4800a27e69b36b659c1929a6537e
Andrew Jones <ajones@digitalbazaar.com>
2022-01-17 16:22:08 -0500

    Start vc-generator.

---
+++ vc-generator/index.js
@@ -0,0 +1,11 @@
+/*!
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ */
+
+// this will generate the signed VCs for the test
+const main = async () => {
+
+};
+
+// run main by calling node ./vc-generator
+main();
