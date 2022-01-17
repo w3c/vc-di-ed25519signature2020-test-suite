7a18eac689691e338823ad54aeb7ee509f1a9db5
Andrew Jones <ajones@digitalbazaar.com>
2022-01-17 16:58:19 -0500

    Add getDidKey method with default seed for key.

--- vc-generator/helpers.js
+++ vc-generator/helpers.js
@@ -1,11 +1,13 @@
 /*!
  * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
-
+const didKeyDriver = require('@digitalbazaar/did-method-key').driver();
 const {writeFile} = require('fs');
 const {promisify} = require('util');
+const {decodeSecretKeySeed} = require('bnid');
 
 const asyncWriteFile = promisify(writeFile);
+const _seed = 'z1AYMku6XEB5KV3XJbYzz9VejGJYRuqzu5wmq4JDRyUCjr8';
 
 /**
  * Writes a json file to disc.
@@ -16,9 +18,25 @@ const asyncWriteFile = promisify(writeFile);
  *
  * @returns {Promise} Resolves on write.
  */
-async function writeJSON({path, data}) {
+const writeJSON = async ({path, data}) => {
   return asyncWriteFile(path, JSON.stringify(data, null, 2));
-}
+};
 
-module.exports = {writeJSON};
+/**
+ * Takes in a bs58 mutlicodec multibase seed and returns a did key.
+ *
+ * @param {object} options - Options to use.
+ * @param {string} [options.keySeed=_seed] - A bs58 encoded string.
+ *
+ * @returns {Promise<object>} - Returns the resulting did key driver result.
+ */
+const getDiDKey = async ({keySeed = _seed} = {}) => {
+  const seed = decodeSecretKeySeed({secretKeySeed: keySeed});
+  return didKeyDriver.generate({seed});
+};
+
+module.exports = {
+  getDiDKey,
+  writeJSON
+};
 
