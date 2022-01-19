e358396701ea5e2ab23409815aafff8fe03df5eb
Andrew Jones <ajones@digitalbazaar.com>
2022-01-19 13:12:19 -0500

    Add documentLoader, working VC issue, & correct exampleVC.

---
+++ vc-generator/documentLoader.js
@@ -0,0 +1,15 @@
+/**
+ *
+ *  Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+*/
+
+const {JsonLdDocumentLoader} = require('jsonld-document-loader');
+const {contextMap} = require('./contexts');
+const jdl = new JsonLdDocumentLoader();
+
+// add contexts to documentLoad
+for(const [key, value] of contextMap) {
+  jdl.addStatic(key, value);
+}
+
+module.exports = jdl.build();
--- vc-generator/index.js
+++ vc-generator/index.js
@@ -3,14 +3,26 @@
  */
 
 const {
+  cloneJSON,
   getDiDKey,
   writeJSON
 } = require('./helpers');
-const vc = require('testVC');
+const credential = require('./testVC');
+const Ed25519Signature2020 = require('./TestEd25519Signature2020');
+const vc = require('@digitalbazaar/vc');
+const documentLoader = require('./documentLoader');
 
 // this will generate the signed VCs for the test
 const main = async () => {
-  const key = await getDiDKey();
+  const {methodFor} = await getDiDKey();
+  const key = methodFor({purpose: 'capabilityInvocation'});
+  const suite = new Ed25519Signature2020({key});
+  const signedVC = await vc.issue({
+    credential: cloneJSON(credential),
+    suite,
+    documentLoader
+  });
+  console.log(JSON.stringify(signedVC, null, 2));
 };
 
 // run main by calling node ./vc-generator
