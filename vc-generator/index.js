c5312955c405da721c4faaf5379920a0a2ff6f2d
Andrew Jones <ajones@digitalbazaar.com>
2022-01-19 13:57:42 -0500

    Start writing valid vc.s

--- vc-generator/index.js
+++ vc-generator/index.js
@@ -11,19 +11,26 @@ const credential = require('./testVC');
 const Ed25519Signature2020 = require('./TestEd25519Signature2020');
 const vc = require('@digitalbazaar/vc');
 const documentLoader = require('./documentLoader');
+const {join} = require('path');
+
+const credentialsPath = join(process.cwd(), 'credentials');
 
 // this will generate the signed VCs for the test
 const main = async () => {
   const {methodFor} = await getDiDKey();
   const key = methodFor({purpose: 'capabilityInvocation'});
+  await _validVC(key);
+};
+
+async function _validVC(key) {
   const suite = new Ed25519Signature2020({key});
-  const signedVC = await vc.issue({
+  const data = await vc.issue({
     credential: cloneJSON(credential),
     suite,
     documentLoader
   });
-  console.log(JSON.stringify(signedVC, null, 2));
-};
+  await writeJSON({path: `${credentialsPath}/validVC.json`, data});
+}
 
 // run main by calling node ./vc-generator
 main();
