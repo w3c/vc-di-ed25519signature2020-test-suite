52d544daa2ac9be4e621e673bde90793d6eb3479
Andrew Jones <ajones@digitalbazaar.com>
2022-03-08 10:00:25 -0600

    Prefer requests over credentials for test data names.

--- tests/Implementation.js
+++ tests/Implementation.js
@@ -51,25 +51,6 @@ class Implementation {
       throw e;
     }
   }
-  async verify({body, headers = {}}) {
-    try {
-      const result = await httpClient.post(
-        this.settings.verifier.endpoint, {
-          headers: {..._headers, ...headers},
-          agent: httpsAgent,
-          json: body
-        }
-      );
-      return result;
-    } catch(e) {
-      console.error(e);
-      // this is just to make debugging easier
-      if(e && e.response && e.response.data) {
-        throw new Error(JSON.stringify(e.response.data, null, 2));
-      }
-      throw e;
-    }
-  }
   async request({body, headers, url, method}) {
     try {
       const result = await httpClient(url, {
