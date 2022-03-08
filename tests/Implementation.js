9fb3516511bae7ba090765e1ce304427afddd0df
Andrew Jones <ajones@digitalbazaar.com>
2022-03-08 09:51:18 -0600

    Use implementation.request over more verbose methods.

--- tests/Implementation.js
+++ tests/Implementation.js
@@ -70,6 +70,25 @@ class Implementation {
       throw e;
     }
   }
+  async request({body, headers, url, method}) {
+    try {
+      const result = await httpClient(url, {
+        method,
+        headers: {..._headers, ...headers},
+        agent: httpsAgent,
+        json: body
+      }
+      );
+      return result;
+    } catch(e) {
+      console.error(e);
+      // this is just to make debugging easier
+      if(e && e.response && e.response.data) {
+        throw new Error(JSON.stringify(e.response.data, null, 2));
+      }
+      throw e;
+    }
+  }
 }
 
 module.exports = Implementation;
