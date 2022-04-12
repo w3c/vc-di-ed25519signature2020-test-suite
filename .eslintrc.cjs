830865d8b9549e6ca38498e1433ca9b35452ec3f
Andrew Jones <ajones@digitalbazaar.com>
2022-04-12 16:25:33 -0500

    Add parser options to .eslintrc.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,10 @@ module.exports = {
   env: {
     node: true
   },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   extends: [
     'eslint-config-digitalbazaar',
     'eslint-config-digitalbazaar/jsdoc'
