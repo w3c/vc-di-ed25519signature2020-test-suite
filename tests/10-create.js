83fd34f78b8cbcc5081b984e35b727d84491ebc7
Andrew Jones <ajones@digitalbazaar.com>
2022-04-12 16:50:00 -0500

    Remove unused test file & start implementing create tests.

--- tests/10-create.js
+++ tests/10-create.js
@@ -12,6 +12,9 @@ const {
 
 const should = chai.should();
 
+// multiple test suite names violate max-len
+/* eslint-disable max-len */
+
 describe('Ed25519Signature2020 (create)', function() {
   for(const [name, implementation] of implementations) {
     const issuer = implementation.issuers.find(issuer =>
@@ -39,11 +42,20 @@ describe('Ed25519Signature2020 (create)', function() {
       });
       */
       describe('Ed25519Signature2020', function() {
-        it('`type` field MUST be the string `Ed25519Signature2020`.', function() {
-
+        const {proof} = issuedVC;
+        const proofs = Array.isArray(proof) ? proof : [proof];
+        it('`type` field MUST be the string `Ed25519Signature2020`.', () => {
+          proofs.some(proof => proof?.type === 'Ed25519Signature2020').to.equal(
+            true,
+            'Expected a "proof.type" to be "Ed25519Signature2020"'
+          );
         });
         it('`proofValue` field MUST exist and be a Multibase-encoded base58-btc value', function() {
-
+          const bs58Multibase = 'z6Mk';
+          proofs.some(proof => proof?.proofValue.startsWith(bs58Multibase)).to.equal(
+            true,
+            'Expected a "proof.proofValue" to be bs58 multibase-encoded.'
+          );
         });
         it('`proofValue` field, when decoded to raw bytes, MUST be 64 bytes in length ' +
           'if the associated public key is 32 bytes in length, or 114 bytes in length ' +
