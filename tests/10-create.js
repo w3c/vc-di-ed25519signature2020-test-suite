36f417a0902da8c2bdcf3f53d9d717c2558bce5b
Andrew Jones <ajones@digitalbazaar.com>
2022-04-15 16:47:12 -0500

    Add test that asserts on key and proofValue length.

--- tests/10-create.js
+++ tests/10-create.js
@@ -9,13 +9,18 @@ const {validVC} = require('../credentials');
 const {
   checkDataIntegrityProofFormat
 } = require('data-integrity-test-suite-assertion');
-const {filterMap} = require('./helpers');
+const {
+  filterMap,
+  getPublicKeyBytes,
+  bs58Bytes
+} = require('./helpers');
 
 const predicate = ({value}) =>
   value.issuers.some(issuer => issuer.tags.has('Ed25519Signature2020'));
 // only use implementations that use `Ed25519 2020`
 const filtered = filterMap({map: implementations, predicate});
 const should = chai.should();
+const bs58 = /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]+$/;
 
 // multiple test suite names violate max-len
 /* eslint-disable max-len */
@@ -38,7 +43,7 @@ describe('Ed25519Signature2020 (create)', function() {
     });
     describe(name, function() {
 
-      checkDataIntegrityProofFormat({getData: () => issuedVC, vendorName: name, proofs});
+      checkDataIntegrityProofFormat({getData: () => issuedVC, vendorName: name});
 
       describe('Ed25519Signature2020', function() {
         it('`type` field MUST be the string `Ed25519Signature2020`.', function() {
@@ -49,7 +54,6 @@ describe('Ed25519Signature2020 (create)', function() {
         });
         it('`proofValue` field MUST exist and be a Multibase-encoded base58-btc value', function() {
           const multibase = 'z';
-          const bs58 = /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]+$/;
           proofs.some(proof => {
             const value = proof?.proofValue || '';
             return value.startsWith(multibase) && bs58.test(value);
@@ -60,10 +64,26 @@ describe('Ed25519Signature2020 (create)', function() {
         });
         it('`proofValue` field, when decoded to raw bytes, MUST be 64 bytes in length ' +
           'if the associated public key is 32 bytes in length, or 114 bytes in length ' +
-          'if the public key is 57 bytes in length.', function() {
-             throw new Error('IMPLEMENT THIS TEST');
+          'if the public key is 57 bytes in length.', async function() {
+          should.exist(issuedVC, 'Expected issuer to have issued a VC.');
+          should.exist(proofs, 'Expected VC to have a proof.');
+          const ed25519Proofs = proofs.filter(proof => proof?.type === 'Ed25519Signature2020');
+          ed25519Proofs.length.should.be.gte(1, 'Expected at least one Ed25519 proof.');
+          for(const proof of ed25519Proofs) {
+            should.exist(proof.proofValue, 'Expected a proof value on the proof.');
+            const valueBytes = bs58Bytes({id: proof.proofValue});
+            should.exist(proof.verificationMethod);
+            const vmBytes = await getPublicKeyBytes({did: proof.verificationMethod});
+            vmBytes.byteLength.should.be.oneOf([32, 57], 'Expected public key bytes to be either 32 or 57 bytes.');
+            if(vmBytes.byteLength === 32) {
+              valueBytes.byteLength.should.equal(64, 'Expected 64 byte proofValue for 32 byte key.');
+            } else {
+              valueBytes.byteLength.should.equal(114, 'Expected 114 byte proofValue for 57 byte key.');
+            }
+          }
         });
         it('proof MUST verify when using a conformant verifier.', async function() {
+          should.exist(verifier, 'Expected implementation to have a VC HTTP API compatible verifier.');
           const {result, error} = await verifier.verify({body: {
             verifiableCredential: issuedVC,
             options: {checks: ['proof']}
