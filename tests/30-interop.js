6d50163938479d5cf49d5d8c6f9111b9b922d842
Andrew Jones <ajones@digitalbazaar.com>
2022-04-14 20:53:08 -0500

    Remove unused files.

--- tests/30-interop.js
+++ tests/30-interop.js
@@ -4,17 +4,7 @@
 'use strict';
 
 const chai = require('chai');
-const Implementation = require('./Implementation');
-const implementations = require('../implementations');
-
-const should = chai.should();
-// test these implementations' issuers or verifiers
-const test = [
-  'Digital Bazaar'
-];
-
-// only test listed implementations
-const testAPIs = implementations.filter(v => test.includes(v.name));
+const {implementations} = require('vc-api-test-suite-implementations');
 
 describe('Ed25519Signature2020 (interop)', function() {
   it('TODO: NxN interop tests', async function() {
