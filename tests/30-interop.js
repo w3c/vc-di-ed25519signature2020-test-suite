0bfb2556e812ae4a325f46da60041e489a573aa0
Andrew Jones <ajones@digitalbazaar.com>
2022-04-19 23:16:46 -0500

    Add working interop test.

--- tests/30-interop.js
+++ tests/30-interop.js
@@ -15,49 +15,47 @@ const predicate = ({value}) =>
 const filtered = filterMap({map: implementations, predicate});
 
 describe('Ed25519Signature2020 (interop)', function() {
-  it('interop tests', async function() {
-    // column names for the matrix go here
-    const columnNames = [];
-    // this will tell the report
-    // to make an interop matrix with this suite
-    this.matrix = true;
-    this.report = true;
-    this.columns = columnNames;
-    this.rowLabel = 'Issuer';
-    this.columnLabel = 'Verifier';
-    for(const [issuerName, {issuers}] of filtered) {
-      let issuedVC;
-      before(async function() {
-        const issuer = issuers.find(issuer =>
-          issuer.tags.has('Ed25519Signature2020'));
-        const body = {credential: {...validVC}};
-        const {result = {}} = await issuer.issue({body});
-        issuedVC = result.data?.verifiableCredential;
-      });
-      for(const [verifierName, {verifiers}] of implementations) {
-        columnNames.push(verifierName);
-        const verifier = verifiers.find(verifier =>
-          verifier.tags.has('VC-HTTP-API'));
+  // column names for the matrix go here
+  const columnNames = [];
+  // this will tell the report
+  // to make an interop matrix with this suite
+  this.matrix = true;
+  this.report = true;
+  this.columns = columnNames;
+  this.rowLabel = 'Issuer';
+  this.columnLabel = 'Verifier';
+  for(const [issuerName, {issuers}] of filtered) {
+    let issuedVC;
+    before(async function() {
+      const issuer = issuers.find(issuer =>
+        issuer.tags.has('Ed25519Signature2020'));
+      const body = {credential: {...validVC}};
+      const {result = {}} = await issuer.issue({body});
+      issuedVC = result.data?.verifiableCredential;
+    });
+    for(const [verifierName, {verifiers}] of implementations) {
+      columnNames.push(verifierName);
+      const verifier = verifiers.find(verifier =>
+        verifier.tags.has('VC-HTTP-API'));
 
-        it(`${verifierName} should verify ${issuerName}`, async function() {
-          this.test.cell = {rowId: issuerName, columnId: verifierName};
-          const body = {
-            verifiableCredential: issuedVC,
-            options: {
-              checks: ['proof']
-            }
-          };
-          const {result, error} = await verifier.verify({body});
-          should.not.exist(error, 'Expected verifier to not error.');
-          should.exist(result, 'Expected result from verifier.');
-          should.exist(result.status, 'Expected verifier to return an HTTP' +
-            'Status code');
-          result.status.should.equal(
-            200,
-            'Expected HTTP Status code 200.'
-          );
-        });
-      }
+      it(`${verifierName} should verify ${issuerName}`, async function() {
+        this.test.cell = {rowId: issuerName, columnId: verifierName};
+        const body = {
+          verifiableCredential: issuedVC,
+          options: {
+            checks: ['proof']
+          }
+        };
+        const {result, error} = await verifier.verify({body});
+        should.not.exist(error, 'Expected verifier to not error.');
+        should.exist(result, 'Expected result from verifier.');
+        should.exist(result.status, 'Expected verifier to return an HTTP' +
+          'Status code');
+        result.status.should.equal(
+          200,
+          'Expected HTTP Status code 200.'
+        );
+      });
     }
-  });
+  }
 });
