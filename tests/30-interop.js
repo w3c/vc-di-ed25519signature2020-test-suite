97ee23f50ed33d32d5a71a56845a05f37ead52c8
Andrew Jones <ajones@digitalbazaar.com>
2022-04-19 21:30:20 -0500

    Start interop tests.

--- tests/30-interop.js
+++ tests/30-interop.js
@@ -5,8 +5,59 @@
 
 const chai = require('chai');
 const {implementations} = require('vc-api-test-suite-implementations');
+const {validVC} = require('../credentials');
+const {filterMap} = require('./helpers');
+
+const should = chai.should();
+const predicate = ({value}) =>
+  value.issuers.some(issuer => issuer.tags.has('Ed25519Signature2020'));
+// only use implementations that use `Ed25519 2020`
+const filtered = filterMap({map: implementations, predicate});
 
 describe('Ed25519Signature2020 (interop)', function() {
-  it('TODO: NxN interop tests', async function() {
+  it('interop tests', async function() {
+    // column names for the matrix go here
+    const columnNames = [];
+    // this will tell the report
+    // to make an interop matrix with this suite
+    this.matrix = true;
+    this.report = true;
+    this.columns = columnNames;
+    this.rowLabel = 'Issuer';
+    this.columnLabel = 'Verifier';
+    for(const [issuerName, {issuers}] of filtered) {
+      let issuedVC;
+      before(async function() {
+        const issuer = issuers.find(issuer =>
+          issuer.tags.has('Ed25519Signature2020'));
+        const body = {credential: {...validVC}};
+        const {result = {}} = await issuer.issue({body});
+        issuedVC = result.data?.verifiableCredential;
+      });
+      for(const [verifierName, {verifiers}] of implementations) {
+        columnNames.push(verifierName);
+        const verifier = verifiers.find(verifier =>
+          verifier.tags.has('VC-HTTP-API'));
+
+        it(`${verifierName} should verify ${issuerName}`, async function() {
+          this.test.cell = {rowId: issuerName, columnId: verifierName};
+          const body = {
+            verifiableCredential: issuedVC,
+            options: {
+              checks: ['proof']
+            }
+          };
+          const {result, error} = await verifier.verify({body});
+          should.not.exist(error, 'Expected verifier to not error.');
+          should.exist(result, 'Expected result from verifier.');
+          should.exist(result.status, 'Expected verifier to return an HTTP' +
+            'Status code');
+          result.status.should.equal(
+            200,
+            'Expected HTTP Status code 200.'
+          );
+        });
+      }
+    }
   });
 });
