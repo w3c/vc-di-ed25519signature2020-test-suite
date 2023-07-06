e34e3a201d6675f77e37ae4f2863c56154b549d9
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-07-05 22:33:10 -0400

    Remove general di issuer and verifier tests.

--- tests/20-verify.js
+++ tests/20-verify.js
@@ -3,12 +3,15 @@
  */
 import {bs58Decode, bs58Encode} from './helpers.js';
 import {verificationFail, verificationSuccess} from './assertions.js';
-import {filterByTag} from 'vc-api-test-suite-implementations';
+import {
+  checkDataIntegrityProofVerifyErrors
+} from 'data-integrity-test-suite-assertion';
+import {endpoints} from 'vc-api-test-suite-implementations';
 import {generateTestData} from './vc-generator/index.js';
 import {klona} from 'klona';
 
 // only use implementations with `Ed25519 2020` verifiers.
-const {match, nonMatch} = filterByTag({
+const {match, nonMatch} = endpoints.filterByTag({
   tags: ['Ed25519Signature2020'],
   property: 'verifiers'
 });
@@ -23,84 +26,11 @@ describe('Ed25519Signature2020 (verify)', function() {
     incorrectCannonization = credentials.get('canonizeJcs');
     incorrectHash = credentials.get('digestSha512');
   });
-  describe('Data Integrity (verifier)', function() {
-    // this will tell the report
-    // to make an interop matrix with this suite
-    this.matrix = true;
-    this.report = true;
-    this.rowLabel = 'Test Name';
-    this.columnLabel = 'Verifier';
-    this.implemented = [...match.keys()];
-    this.notImplemented = [...nonMatch.keys()];
-    for(const [name, implementation] of match) {
-      describe(name, function() {
-      // wrap the testApi config in an Implementation class
-        const verifier = implementation.verifiers.find(verifier =>
-          verifier.tags.has('Ed25519Signature2020'));
-        it('If the "proof" field is missing or invalid, a MALFORMED error ' +
-          'MUST be returned.', async function() {
-          this.test.cell = {
-            columnId: name,
-            rowId: this.test.title
-          };
-          const credential = klona(issuedVc);
-          delete credential.proof;
-          await verificationFail({credential, verifier});
-        });
-        it('If the "type" field is missing or invalid, a MALFORMED error ' +
-          'MUST be returned.', async function() {
-          this.test.cell = {
-            columnId: name,
-            rowId: this.test.title
-          };
-          const credential = klona(issuedVc);
-          delete credential.proof.type;
-          await verificationFail({credential, verifier});
-        });
-        it('If the "created" field is missing or invalid, a MALFORMED error ' +
-          'MUST be returned.', async function() {
-          this.test.cell = {
-            columnId: name,
-            rowId: this.test.title
-          };
-          const credential = klona(issuedVc);
-          delete credential.proof.created;
-          await verificationFail({credential, verifier});
-        });
-        it('If the "verificationMethod" field is missing or invalid, ' +
-          'a MALFORMED error MUST be returned.', async function() {
-          this.test.cell = {
-            columnId: name,
-            rowId: this.test.title
-          };
-          const credential = klona(issuedVc);
-          delete credential.proof.verificationMethod;
-          await verificationFail({credential, verifier});
-        });
-        it('If the "proofPurpose" field is missing or invalid, ' +
-          'a MALFORMED error MUST be returned.', async function() {
-          this.test.cell = {
-            columnId: name,
-            rowId: this.test.title
-          };
-          const credential = klona(issuedVc);
-          delete credential.proof.proofPurpose;
-          await verificationFail({credential, verifier});
-        });
-        it('If the "proofValue" field is missing or invalid, ' +
-          'a MALFORMED error MUST be returned.', async function() {
-          this.test.cell = {
-            columnId: name,
-            rowId: this.test.title
-          };
-          const credential = klona(issuedVc);
-          delete credential.proof.proofValue;
-          await verificationFail({credential, verifier});
-        });
-
-      });
-    }
+  checkDataIntegrityProofVerifyErrors({
+    implemented: match,
+    notImplemented: nonMatch
   });
+
   describe('Ed25519Signature2020 (verifier)', function() {
     // this will tell the report
     // to make an interop matrix with this suite
@@ -111,11 +41,10 @@ describe('Ed25519Signature2020 (verify)', function() {
     this.implemented = [...match.keys()];
     this.notImplemented = [...nonMatch.keys()];
 
-    for(const [name, implementation] of match) {
+    for(const [name, {implementation}] of match) {
       describe(name, function() {
-      // wrap the testApi config in an Implementation class
-        const verifier = implementation.verifiers.find(verifier =>
-          verifier.tags.has('Ed25519Signature2020'));
+        const verifier = implementation.verifiers.find(
+          verifier => verifier.tags.has('Ed25519Signature2020'));
         it('MUST verify a valid VC with an Ed25519Signature2020 proof',
           async function() {
             this.test.cell = {
@@ -125,27 +54,6 @@ describe('Ed25519Signature2020 (verify)', function() {
             const credential = klona(issuedVc);
             await verificationSuccess({credential, verifier});
           });
-        it('If the "type" field is not the string "Ed25519Signature2020", an ' +
-          'UNKNOWN_CRYPTOSUITE_TYPE error MUST be returned.', async function() {
-          this.test.cell = {
-            columnId: name,
-            rowId: this.test.title
-          };
-          const credential = klona(issuedVc);
-          credential.proof.type = 'UnknownCryptoSuite';
-          await verificationFail({credential, verifier});
-        });
-        it('If the "proofValue" field is not a multibase-encoded base58-btc ' +
-          'value, an INVALID_PROOF_VALUE error MUST be returned.',
-        async function() {
-          this.test.cell = {
-            columnId: name,
-            rowId: this.test.title
-          };
-          const credential = klona(issuedVc);
-          credential.proof.proofValue = 'not-multibase-bs58-encoded!!';
-          await verificationFail({credential, verifier});
-        });
         it('If the "proofValue" field, when decoded to raw bytes, is not ' +
           '64 bytes in length if the associated public key is 32 bytes ' +
           'in length, or 114 bytes in length if the public key is 57 bytes ' +
