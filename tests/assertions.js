f883c761ea2177d4afc34852f28a2b993f97e5d6
Patrick <patrick.st-louis@opsecid.ca>
2024-07-02 19:20:11 +0000

    add 422 assertions
    Signed-off-by: Patrick <patrick.st-louis@opsecid.ca>

--- tests/assertions.js
+++ tests/assertions.js
@@ -49,10 +49,8 @@ export const verificationFail = async ({credential, verifier}) => {
   should.not.exist(result, 'Expected no result from verifier.');
   should.exist(error, 'Expected verifier to error.');
   should.exist(error.status, 'Expected verifier to return an HTTP Status code');
-  error.status.should.equal(
-    400,
-    'Expected HTTP Status code 400 invalid input!'
-  );
+  error.status.should.be.oneOf([400, 422],
+    'Expected HTTP Status code 400 or 422.');
 };
 
 export const verificationSuccess = async ({credential, verifier}) => {
